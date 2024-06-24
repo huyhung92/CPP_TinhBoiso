07c2f74be2bfae9c886b6e254fd08d9d6721b885
SPC <SPC@33-LAB402>
2024-06-24 19:21:38 +0700

    Add project files.

---
+++ CPP_TinhBoiso.cpp
@@ -0,0 +1,22 @@
+// CPP_TinhBoiso.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Chuong trinh tinh boi so";
+	int n, b;
+	cout << "Nhap so: ";
+	cin >> n;
+	for (int i = 1; i <= 10; i++) {
+		cout << "  " << n * i;
+
+	}
+	return 0;
+
+}
+
+
+  
